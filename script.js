6fb9a20beae1b1d5d81919f371d657f0ce3a0919
MD. ARIFUL ISLAM <arifhstuece@gmail.com>
2022-03-14 16:18:43 +0600

    update profile

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Microsoft Office Applications", "Microsoft Word", "Microsoft Exel", "Microsoft PowerPoint", "Data Entry"],
+        strings: ["Microsoft Word", "Microsoft Exel", "Microsoft PowerPoint", "Data Entry"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Co-Executive of NBICT LAB|"],
+        strings: ["Intern at NBICT LAB|"],
         typeSpeed: 100,
         backSpeed: 40,
         loop: true
